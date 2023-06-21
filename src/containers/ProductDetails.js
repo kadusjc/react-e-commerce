34ccce8e6d64808ec47d985e8d9560057322ec48
Eng. Kadu <kadusjc@yahoo.com.br>
2023-06-21 14:46:08 -0300

    Adicionando o add to cart dentro do detalhes

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Card } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Card, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
+import { addToCart } from '../redux/actions/cartActions';
+import { CartPlusFill } from "react-bootstrap-icons";
 
 const ProductDetails = () => {
   const { productId } = useParams()
-
+  const userProfile = useSelector((state) => state.user.user);
+  
   let product = null
   const products = useSelector((state) => state.products.products)
   if (productId) product = products.find((p) => p.id.toString() === productId);
   
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+    navigate("/cart")
+  };
 
   if (!product) {
     return <div>Produto nao encontrado.</div>;
@@ -27,7 +39,11 @@ const ProductDetails = () => {
           </Card.Title>
           <Card.Text>Descrição: {product.description}</Card.Text>
           <Card.Text>Preço: ${product.price}</Card.Text>
-          
+          { userProfile && userProfile.name && 
+          <Button style={{margin: '2px'}} variant="secondary" onClick={() => handleAddToCart(product)}>
+            <CartPlusFill/>&nbsp;Adic. Carrinho
+          </Button>
+        }
         </Card.Body>
       </Card>
     </div>
