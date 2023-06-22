3ea4e702787a44007d73d0032bf0c58aefc85476
Eng. Kadu <kadusjc@yahoo.com.br>
2023-06-22 20:23:19 -0300

    Primeiro teste com redux funcionando

--- src/components/CartItem.test.js
+++ src/components/CartItem.test.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import CartItem from './CartItem';
 import { shallow } from 'enzyme';
-import toJson from "enzyme-to-json"
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+
+import CartItem from './CartItem';
+import { addToCart } from '../redux/actions/cartActions';
+
+const mockStore = configureStore([]);
 
 describe('CartItem', () => {
-  const mockProduct = {
-    id: 1,
-    name: 'Example Product',
-    price: 10.99,
-    quantity: 2,
-  };
+  let store = mockStore({ cart: { cartItems: [] } })
+  const mockProduct = { id: 1, name: 'Example Product', price: 10.99, quantity: 2 };
+  
+  beforeEach(() => {
+   store = mockStore({ cart: { cartItems: [] } })
+   //store.dispatch(addToCart(mockProduct))
+  });
 
-  it('should render the component correctly', () => {
-      const app = shallow(<CartItem product={mockProduct} />)
-      app.instance().setState({data: <li>teste</li>});
-      app.instance().forceUpdate();
-      expect(app.find('li')).toHaveLength(1)
+  it('should render the component correctly with message "Seu Carrinho está vazio"', () => {
+      const wrapper = shallow(<Provider store={store}><CartItem product={mockProduct} /></Provider>)
+      expect(wrapper.html()).toBe('<div><p>Seu carrinho está Vazio.</p></div>')
     })
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,7 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import React from "react" 
+
+React.useLayoutEffect = React.useEffect //https://stackoverflow.com/questions/58070996/how-to-fix-the-warning-uselayouteffect-does-nothing-on-the-server
+configure({ adapter: new Adapter() });
