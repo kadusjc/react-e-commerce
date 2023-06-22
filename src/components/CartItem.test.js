13c98ba07d4cb8052d291b648300cf5e05786abe
Eng. Kadu <kadusjc@yahoo.com.br>
2023-06-22 20:31:49 -0300

    Primeiro teste quase pronto

--- src/components/CartItem.test.js
+++ src/components/CartItem.test.js
@@ -4,21 +4,24 @@ import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
 import CartItem from './CartItem';
-import { addToCart } from '../redux/actions/cartActions';
 
 const mockStore = configureStore([]);
 
 describe('CartItem', () => {
   let store = mockStore({ cart: { cartItems: [] } })
-  const mockProduct = { id: 1, name: 'Example Product', price: 10.99, quantity: 2 };
+  const mockProduct = { id: 1, name: 'Example Product', price: 10.99, qty: 2 };
   
   beforeEach(() => {
    store = mockStore({ cart: { cartItems: [] } })
-   //store.dispatch(addToCart(mockProduct))
   });
 
   it('should render the component correctly with message "Seu Carrinho está vazio"', () => {
-      const wrapper = shallow(<Provider store={store}><CartItem product={mockProduct} /></Provider>)
-      expect(wrapper.html()).toBe('<div><p>Seu carrinho está Vazio.</p></div>')
-    })
+    const wrapper = shallow(<Provider store={store}><CartItem /></Provider>)
+    expect(wrapper.html()).toBe('<div><p>Seu carrinho está Vazio.</p></div>')
+  })
+
+  it.skip('should render the Cart Item component HTML code correctly', () => {
+    const wrapper = shallow(<Provider store={store}><CartItem item={mockProduct} /></Provider>)
+    expect(wrapper.find('div').text()).toContain('text to check')
+  })
 });
