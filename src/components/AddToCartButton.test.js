15caae9b7d342a5007bdf01959020a4650db6516
Eng. Kadu <kadusjc@yahoo.com.br>
2023-06-23 16:36:50 -0300

    testando

---
+++ src/components/AddToCartButton.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, mount } from 'enzyme';
+
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+
+import AddToCartButton from './AddToCartButton';
+import { addToCart } from '../redux/actions/cartActions';
+
+const mockStore = configureStore([]);
+
+describe('AddToCartButton', () => {
+  let store = mockStore({ cart: { cartItems: [] } })
+  const mockProduct = { id: 1, name: 'Example Product', price: 10.99, qty: 2 };
+  
+  beforeEach(() => {
+    store = mockStore({ user: { user: null } })
+  });
+
+  it('should NOT display the Add to the Cart Button if user is NOT logged', () => {
+    const wrapper = render(<Provider store={store}><BrowserRouter><AddToCartButton product={mockProduct}/></BrowserRouter></Provider>)
+    expect(wrapper.text()).toBe('')
+  })
+
+  it('should render the Add to the Cart Button just if user is logged', () => {
+    store = mockStore({ user: { user: { name: 'Carlos E. F Correa', email: 'kadusjc1984@gmail.com'} } })
+    const wrapper = render(<Provider store={store}><BrowserRouter><AddToCartButton product={mockProduct}/></BrowserRouter></Provider>)
+    expect(wrapper.text()).toBe(' Adic. Carrinho')
+  })
+  
+  it('should call Redux Action "addToCart", with Product, when the button is clicked', () => {
+    store = mockStore({ user: { user: { name: 'Carlos E. F Correa', email: 'kadusjc1984@gmail.com'} } })
+    const wrapper = mount(<Provider store={store}><BrowserRouter><AddToCartButton product={mockProduct}/></BrowserRouter></Provider>)
+    wrapper.find('button').simulate('click')
+
+    const expectedPayload = { payload: { ...mockProduct }, type: 'ADD_TO_CART'} 
+    expect(store.getActions()).toEqual([expectedPayload])
+  })
+});
--- src/components/CartItem.test.js
+++ src/components/CartItem.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
@@ -12,16 +12,17 @@ describe('CartItem', () => {
   const mockProduct = { id: 1, name: 'Example Product', price: 10.99, qty: 2 };
   
   beforeEach(() => {
-   store = mockStore({ cart: { cartItems: [] } })
+    store = mockStore({ cart: { cartItems: [] } })
   });
 
   it('should render the component correctly with message "Seu Carrinho está vazio"', () => {
-    const wrapper = shallow(<Provider store={store}><CartItem /></Provider>)
-    expect(wrapper.html()).toBe('<div><p>Seu carrinho está Vazio.</p></div>')
+    const wrapper = render(<Provider store={store}><CartItem /></Provider>)
+    expect(wrapper.text()).toBe('Seu carrinho está Vazio.')
   })
 
-  it.skip('should render the Cart Item component HTML code correctly', () => {
-    const wrapper = shallow(<Provider store={store}><CartItem item={mockProduct} /></Provider>)
-    expect(wrapper.find('div').text()).toContain('text to check')
+  it('should render the Cart Item with one item when code correctly when cartItems is not empty', () => {
+    store = mockStore({ cart: { cartItems: [ mockProduct ] } })
+    const wrapper = render(<Provider store={store}><CartItem item={mockProduct} /></Provider>)
+    expect(wrapper.text()).toContain('Nome Example Product - Preço $10.99 - Qtd 2   Remover')
   })
 });
